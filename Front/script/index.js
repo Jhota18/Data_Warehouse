a45402cd85b69c418ba5fc03b8c77b842fe1447d
Jhonatan Gomez <jhonatangomez_18@hotmail.com>
2020-11-06 01:24:31 -0500

    Falta filtro por interes

--- Front/script/index.js
+++ Front/script/index.js
@@ -1,5 +1,4 @@
 
-
 function sortByNameA() {
     var table, rows, switching, i, x, y, shouldSwitch;
     table = document.getElementById("myTable");
@@ -9,7 +8,7 @@ function sortByNameA() {
       switching = false;
       rows = document.querySelectorAll('.contact');
       
-      for (i = 0; i < (rows.length - 1); i++) {
+      for (i = 0; i < rows.length; i++) {
         
         shouldSwitch = false;
         x = rows[i].getElementsByTagName("h3")[0];
@@ -35,11 +34,169 @@ function sortByNameZ() {
       switching = false;
       rows = document.querySelectorAll('.contact');
       
-      for (i = 0; i < (rows.length - 1); i++) {
+      for (i = 0; i < rows.length; i++) {
         
         shouldSwitch = false;
         x = rows[i].getElementsByTagName("h3")[0];
         y = rows[i + 1].getElementsByTagName("h3")[0];
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+          shouldSwitch = true;
+          break;
+        }
+      }
+      if (shouldSwitch) {
+        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+        switching = true;
+      }
+    }
+}
+
+function sortByCountryA() {
+    var table, rows, switching, i, x, y, shouldSwitch;
+    table = document.getElementById("myTable");
+    switching = true;
+    while (switching) {
+        
+      switching = false;
+      rows = document.querySelectorAll('.contact');
+      
+      for (i = 0; i < rows.length; i++) {
+        
+        shouldSwitch = false;
+        x = rows[i].getElementsByTagName("h3")[1];
+        y = rows[i + 1].getElementsByTagName("h3")[1];
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          shouldSwitch = true;
+          break;
+        }
+      }
+      if (shouldSwitch) {
+        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+        switching = true;
+      }
+    }
+}
+
+function sortByCountryZ() {
+    var table, rows, switching, i, x, y, shouldSwitch;
+    table = document.getElementById("myTable");
+    switching = true;
+    while (switching) {
+        
+      switching = false;
+      rows = document.querySelectorAll('.contact');
+      
+      for (i = 0; i < rows.length; i++) {
+        
+        shouldSwitch = false;
+        x = rows[i].getElementsByTagName("h3")[1];
+        y = rows[i + 1].getElementsByTagName("h3")[1];
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+          shouldSwitch = true;
+          break;
+        }
+      }
+      if (shouldSwitch) {
+        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+        switching = true;
+      }
+    }
+}
+
+function sortByCompanyA() {
+    var table, rows, switching, i, x, y, shouldSwitch;
+    table = document.getElementById("myTable");
+    switching = true;
+    while (switching) {
+        
+      switching = false;
+      rows = document.querySelectorAll('.contact');
+      
+      for (i = 0; i < rows.length; i++) {
+        
+        shouldSwitch = false;
+        x = rows[i].getElementsByTagName("h3")[2];
+        y = rows[i + 1].getElementsByTagName("h3")[2];
+        console.log(x);
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          shouldSwitch = true;
+          break;
+        }
+      }
+      if (shouldSwitch) {
+        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+        switching = true;
+      }
+    }
+}
+
+function sortByCompanyZ() {
+    var table, rows, switching, i, x, y, shouldSwitch;
+    table = document.getElementById("myTable");
+    switching = true;
+    while (switching) {
+        
+      switching = false;
+      rows = document.querySelectorAll('.contact');
+      
+      for (i = 0; i < rows.length; i++) {
+        
+        shouldSwitch = false;
+        x = rows[i].getElementsByTagName("h3")[2];
+        y = rows[i + 1].getElementsByTagName("h3")[2];
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+          shouldSwitch = true;
+          break;
+        }
+      }
+      if (shouldSwitch) {
+        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+        switching = true;
+      }
+    }
+}
+
+function sortByRoleA() {
+    var table, rows, switching, i, x, y, shouldSwitch;
+    table = document.getElementById("myTable");
+    switching = true;
+    while (switching) {
+        
+      switching = false;
+      rows = document.querySelectorAll('.contact');
+      
+      for (i = 0; i < rows.length; i++) {
+        
+        shouldSwitch = false;
+        x = rows[i].getElementsByTagName("h3")[3];
+        y = rows[i + 1].getElementsByTagName("h3")[3];
+        console.log(x);
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          shouldSwitch = true;
+          break;
+        }
+      }
+      if (shouldSwitch) {
+        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+        switching = true;
+      }
+    }
+}
+
+function sortByRoleZ() {
+    var table, rows, switching, i, x, y, shouldSwitch;
+    table = document.getElementById("myTable");
+    switching = true;
+    while (switching) {
+        
+      switching = false;
+      rows = document.querySelectorAll('.contact');
+      
+      for (i = 0; i < rows.length; i++) {
+        
+        shouldSwitch = false;
+        x = rows[i].getElementsByTagName("h3")[3];
+        y = rows[i + 1].getElementsByTagName("h3")[3];
         if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
           shouldSwitch = true;
           break;
